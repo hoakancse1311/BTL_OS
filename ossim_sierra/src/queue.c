f670347695e698a567914462fba9cd09ab586a6b
hoangkhanh <khanh.nguyenhoang2005@hcmut.edu.vn>
2025-04-16 17:15:40 +0700

    Update queue.c

--- ossim_sierra/src/queue.c
+++ ossim_sierra/src/queue.c
@@ -2,47 +2,34 @@
 #include <stdlib.h>
 #include "queue.h"
 
-int empty(struct queue_t *q)
-{
-        if (q == NULL)
-                return 1;
-        return (q->size == 0);
+int empty(struct queue_t * q) {
+        if (q == NULL) return 1;
+	return (q->size == 0);
 }
 
-void enqueue(struct queue_t *q, struct pcb_t *proc)
-{
-        /* TODO: put a new process to queue [q] */
-        if (q == NULL || q->size >= MAX_QUEUE_SIZE)
+void enqueue(struct queue_t * q, struct pcb_t * proc) {
+        /* TODO: put a new process to queue [q] */ //done
+        if (q == NULL || proc == NULL || q->size >= MAX_QUEUE_SIZE) {
+                return; 
+        }
+        if (q->size >= MAX_QUEUE_SIZE) {
                 return;
+        }
         q->proc[q->size] = proc;
-        q->size++;
-}
+        q->size++;      
+}       
 
-struct pcb_t *dequeue(struct queue_t *q)
-{
+struct pcb_t * dequeue(struct queue_t * q) {
         /* TODO: return a pcb whose prioprity is the highest
          * in the queue [q] and remember to remove it from q
          * */
-        if (q == NULL || empty(q))
+	// return NULL;
+        if (q->size == 0)
                 return NULL;
-        int highest_prio = 0;
-        for (int i = 0; i < q->size; i++)
-        {
-                if (q->proc[i]->priority > q->proc[highest_prio]->priority)
-                {
-                        highest_prio = i;
-                }
-        }
-        struct pcb_t *proc = q->proc[highest_prio];
-
-        // Remove the process and shift other processes
-        for (int i = highest_prio; i < q->size - 1; i++)
-        {
-                q->proc[i] = q->proc[i + 1];
-        }
-
-        // Update size
+        struct pcb_t *proc = q->proc[0];
+        for (int i = 1; i < q->size; i++)
+                q->proc[i - 1] = q->proc[i];
         q->size--;
-        q->proc[q->size] = NULL;
         return proc;
 }
+
